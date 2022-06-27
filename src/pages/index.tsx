222eb25e5a802054ef7d774ca8889d42098d637b
helton-quintans <helton.quit@gmail.com>
2022-06-27 00:06:31 -0300

    refactor: change of ssr for ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import SubscribeButton from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -36,7 +36,7 @@ export default function Home({ product }: Homeprops) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   //buscando o preço do produto da api do stripe
   const price = await stripe.prices.retrieve("price_1Ks8HuEjUKhia15EuMwe2eUT", {
     //expand trás todas as informações do produto
@@ -54,6 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
-    // revalidate: 60 * 60 * 24, //24h
+    revalidate: 60 * 60 * 24, //24h
   };
 };
